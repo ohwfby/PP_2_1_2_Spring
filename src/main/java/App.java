22a2b3834e319bb4a5b30d4c4d9707d5debd51b7
Mikhail Semenov <haruko298@gmail.com>
2024-09-28 15:25:08 +0300

    Сделал

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCat =
+                (Cat) applicationContext.getBean("meow");
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("meow");
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean.equals(bean1));
+        System.out.println(beanCat.getMessage());
+        System.out.println(beanCat.getMessage());
+        System.out.println(beanCat.equals(beanCat1));
+
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
+
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Bean(name="meow")
+    @Scope("prototype")
+    public Cat getMessage() {
+        Cat pojoCat = new Cat();
+        pojoCat.setMessage("Meow");
+        return pojoCat;
     }
 }
